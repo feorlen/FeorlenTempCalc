670d8e97695e43dfb4856c8208ae8aef0e2c7d16
feorlen <feorlen@Feorlens-MacBook-Pro.local>
2013-10-04 10:31:29 -0700

    Initial Commit

---
+++ FeorlenTempCalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FeorlenTempCalc
+//
+//  Created by Feorlen on 10/4/13.
+//  Copyright (c) 2013 Feorlen.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

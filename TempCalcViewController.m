bb1fdecfb7f5f21c312a6f180ff50d5e7140fab5
feorlen <feorlen@Feorlens-MacBook-Pro.local>
2013-10-08 08:17:31 -0700

    clean up unnecessary stuff, better method names

--- TempCalcViewController.m
+++ TempCalcViewController.m
@@ -10,9 +10,8 @@
 
 @interface TempCalcViewController ()
 
-- (void)convertValue;
-- (void)convertCtoF;
-- (void)convertFtoC;
+- (void)celsiusWithFahrenheit;
+- (void)fahrenheitWithCelsius;
 
 @end
 
@@ -46,47 +45,46 @@
 #pragma mark - UITextField delegate
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    
+    // make the Done button appear
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]
         initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self
         action:@selector(onDoneButton)];
+    
     return YES;
 }
 
 - (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
     self.navigationItem.rightBarButtonItem = nil;
     
+    // call the correct conversion for the field being edited
+    
     if (textField == self.degCfield) {
-        [self convertCtoF];
+        [self celsiusWithFahrenheit];
     }
     else if (textField == self.degFfield) {
-        [self convertFtoC];
+        [self fahrenheitWithCelsius];
     }
     else {
         // shouldn't happen
+        // there could be some error handling here
     }
     return YES;
 }
 
+
 #pragma mark - Private methods
 
+// conversions:
+// read the field, calculate the result, update other field (rounded to int)
 
-// conversion methods - result rounded to nearest integer
-
-- (void)convertValue {
-    // determine which field was edited last
-    // call correct convert method
-    
-    // for test, convert c to f
-    [self convertCtoF];
-}
-
-- (void)convertCtoF {
+- (void)celsiusWithFahrenheit {
     float cValue = [self.degCfield.text floatValue];
     float fValue = (cValue * (9.0/5.0)) + 32.0;
     self.degFfield.text = [NSString stringWithFormat:@"%0.0f", fValue];
 }
 
-- (void)convertFtoC {
+- (void)fahrenheitWithCelsius {
     float fValue = [self.degFfield.text floatValue];
     float cValue = (fValue - 32.0) * (5.0/9.0);
     self.degCfield.text = [NSString stringWithFormat:@"%0.0f", cValue];
@@ -94,6 +92,7 @@
 }
 
 - (void)onDoneButton {
+    // remove the done button when editing is finished
     [self.view endEditing:YES];
 }
 
